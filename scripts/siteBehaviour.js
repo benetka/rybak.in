5089fec60c7f02cdef89cd63a48ee5587b101473
Jenda Rybak <jendarybak@gmail.com>
2013-02-20 01:53:30 +0100

    ipads iphones i...

--- scripts/siteBehaviour.js
+++ scripts/siteBehaviour.js
@@ -4,15 +4,22 @@ Twitter: @jendarybak
 Author URL: http://www.rybak.in
 */
 
-$(document).ready(function(){
+function isMobileBrowser() {
+  var a = navigator.userAgent||navigator.vendor||window.opera;
 
+  return (/android|avantgo|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|midp|mmp|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || 
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)));
+}
+
+$(document).ready(function(){
+	
 	/*
 	 * Background image rotation
 	 */
 	var rand = Math.floor(Math.random()*14);
 	var bgUrl = "url(./images/slide1/jan-rybak-bg" + rand +".jpg)";
 	$("#introBg").css("background-image", bgUrl);
-
+	
 	/*
 	 * Intro fading
 	 */
@@ -47,87 +54,93 @@ $(document).ready(function(){
 		easing: "easeInSine",
 		hash: true 
 	});
-
-	/*
-	 * Skills image handling
-	 */
-
-	/* flag: skills image zoomed (true/false) */
-	var up = false;
-
-	/* original size of skills image */
-	var imgWidth = $('#skills').width();
-	var imgHeight = $('#skills').height();
-
-	/* updated size of image skills */
-	var newWidth = imgWidth;
-	var newHeight = imgHeight;
-
-	if ($(window).width() < imgWidth) {
-		newWidth = Math.floor($(window).width()/1.2);
-		newHeight = imgHeight/(imgWidth/newWidth);
-	}
 	
-	$('#skills').click(function() { 
-		if (up == false) {
-			up = true;
 	
-			$(this).stop().animate({
-				easing: 'Swing',
-		    	top: '-=200',
-				width: newWidth,
-				height: newHeight				
-		  	}, 1500);
+	// Avoid dynamic behaviour for mobile agents that
+	if(isMobileBrowser() == false) {
 
-			$("#aboutMe p").animate(
-				{opacity: 0.2},
-				{duration: 800},
-				{easing: 'Swing'}
-			);
+		/*
+		 * Skills image handling
+		 */
+
+		/* flag: skills image zoomed (true/false) */
+		var up = false;
+
+		/* original size of skills image */
+		var imgWidth = $('#skills').width();
+		var imgHeight = $('#skills').height();
+
+		/* updated size of image skills */
+		var newWidth = imgWidth;
+		var newHeight = imgHeight;
+
+		if ($(window).width() < imgWidth) {
+			newWidth = Math.floor($(window).width()/1.2);
+			newHeight = imgHeight/(imgWidth/newWidth);
+		}
+	
+		$('#skills').click(function() { 
+			if (up == false) {
+				up = true;
+	
+				$(this).stop().animate({
+					easing: 'Swing',
+			    	top: '-=200',
+					width: newWidth,
+					height: newHeight				
+			  	}, 1500);
+
+				$("#aboutMe p").animate(
+					{opacity: 0.2},
+					{duration: 800},
+					{easing: 'Swing'}
+				);
 			
-		} else {
-			up = false;
+			} else {
+				up = false;
 	
-			$(this).stop().animate({
-				width: imgWidth,
-				height: imgHeight,
-		    	top: '+=200'
-		  	}, 300);			
+				$(this).stop().animate({
+					width: imgWidth,
+					height: imgHeight,
+			    	top: '+=200'
+			  	}, 300);			
 		
-			$("#aboutMe p").animate(
-				{opacity: 1}, 
-				{duration: 1300}, 
-				{easing: 'Swing'}
-			);		
-		}
-	});
+				$("#aboutMe p").animate(
+					{opacity: 1}, 
+					{duration: 1300}, 
+					{easing: 'Swing'}
+				);		
+			}
+		});
 	
-	$(window).scroll(function () {
-		if(up == true) {
-			up = false;
+		$(window).scroll(function () {
+			if(up == true) {
+				up = false;
 						
-			$('#skills').stop().animate({
-		    	top: '+=200',
-		  	}, 300);			
+				$('#skills').stop().animate({
+			    	top: '+=200',
+			  	}, 300);			
 		
-			$("#aboutMe p").animate(
-				{opacity: 1}, 
-				{duration: 1300}, 
-				{easing: 'Swing'}
-			);
-		}
-	});
+				$("#aboutMe p").animate(
+					{opacity: 1}, 
+					{duration: 1300}, 
+					{easing: 'Swing'}
+				);
+			}
+		});
 	
-	/*
-	 * Parallax effect
-	 */	
-	// .parallax(xPosition, speedFactor, outerHeight) options:
-	// xPosition - Horizontal position of the element
-	// inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-	// outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+		/*
+		 * Parallax effect
+		 */	
+		// .parallax(xPosition, speedFactor, outerHeight) options:
+		// xPosition - Horizontal position of the element
+		// inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+		// outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
 
-	$('#introBg').parallax("50%", 0.2, null, true);
-	$('#aboutMe article').parallax("50%", 0.8, null, false);
-	$('#skills').parallax("50%", 1.3, null, false);
+		$('#introBg').parallax("50%", 0.2, null, true);
+		$('#aboutMe article').parallax("50%", 0.8, null, false);
+		$('#skills').parallax("50%", 1.3, null, false);
 
+
+	}	
 })
